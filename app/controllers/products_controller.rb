9c93cea39685dc1b83da2eaf6ae604210c0dc9a6
Jungmin Cha <ckwjdals90@gmail.com>
2016-12-07 22:14:24 +0000

    Jungle Project setup completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
+  # ========= defalult method names =========
+  # index - shows all resources of a certain type.
+  # show - show details about a specific resources.
+  # create - receive resources data from a form and store it to the database.
+  # new - display a form to create a new resource.
+  # edit - display a form to edit a resource.
+  # update - receive data from an update form and store it in the database.
+  # destroy - deletes a resource from the database.
 
   def index
     @products = Product.all.order(created_at: :desc)
